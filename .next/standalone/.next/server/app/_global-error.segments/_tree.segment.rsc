594b1aa6710b5cc0bcdd4ed08b9fb8721228523a
christos <kemimeister@gmail.com>
2026-06-01 01:25:17 +0100

    release: v0.3.3

--- .next/standalone/.next/server/app/_global-error.segments/_tree.segment.rsc
+++ .next/standalone/.next/server/app/_global-error.segments/_tree.segment.rsc
@@ -1,1 +1,1 @@
-0:{"tree":{"name":"","param":null,"prefetchHints":64,"slots":{"children":{"name":"__PAGE__","param":null,"prefetchHints":160,"slots":null}}},"staleTime":300,"buildId":"57ff412"}
+0:{"tree":{"name":"","param":null,"prefetchHints":64,"slots":{"children":{"name":"__PAGE__","param":null,"prefetchHints":160,"slots":null}}},"staleTime":300,"buildId":"54b2971"}
--- .next/standalone/.next/server/middleware-build-manifest.js
+++ .next/standalone/.next/server/middleware-build-manifest.js
@@ -7,9 +7,9 @@ globalThis.__BUILD_MANIFEST = {
     "static/chunks/0cz1d0mv5g_q7.js"
   ],
   "lowPriorityFiles": [
-    "static/57ff412/_buildManifest.js",
-    "static/57ff412/_ssgManifest.js",
-    "static/57ff412/_clientMiddlewareManifest.js"
+    "static/54b2971/_buildManifest.js",
+    "static/54b2971/_ssgManifest.js",
+    "static/54b2971/_clientMiddlewareManifest.js"
   ],
   "rootMainFiles": [
     "static/chunks/2rd8t-zexnp9y.js",
